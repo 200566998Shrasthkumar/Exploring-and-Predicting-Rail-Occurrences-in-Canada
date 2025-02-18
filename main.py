76402be695c1e821de4c0cc95f6e1edc81913baa
Steve <code@stvmk.com>
2025-02-18 00:19:09 -0500

    Main code change

--- main.py
+++ main.py
@@ -9,5 +9,3 @@ occurance_dataset = filter_dataset.load_and_filter_dataset('.\\datasets\\COMPONE
 injuries_dataset = filter_dataset.load_and_filter_dataset('.\\datasets\\INJURIES_PUBLIC.csv', 10)
 train_dataset = filter_dataset.load_and_filter_dataset('.\\datasets\\TRAIN_PUBLIC.csv', 1000, 'Fre')
 components_dataset = filter_dataset.load_and_filter_dataset('.\\datasets\\COMPONENTS_PUBLIC.csv', 1000, 'Fre')
-
-print(components_dataset.columns.values)
